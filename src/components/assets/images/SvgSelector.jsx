3cb2a82662b7237fafd5beb096df306f8c2b1492
Rodion <redia1994@mail.ru>
2021-10-09 13:58:57 +0300

    finished

--- src/components/assets/images/SvgSelector.jsx
+++ src/components/assets/images/SvgSelector.jsx
@@ -5,12 +5,12 @@ export const SvgSelector = ({ id }) => {
         case 'plus':
             return (
                 <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M10 1V19M18.9706 10H1" stroke="#D9D9D9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M10 1V19M18.9706 10H1" stroke="#D9D9D9" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                 </svg>
             )
         case 'minus':
             return (
-                <svg width="18" height="2" viewBox="0 0 18 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <svg width="18" height="20" viewBox="0 0 18 2" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <rect width="18" height="2" rx="1" fill="white" />
                 </svg>
             )
@@ -18,3 +18,4 @@ export const SvgSelector = ({ id }) => {
             return null
     }
 }
+
--- src/components/program/ProgramManagment.jsx
+++ src/components/program/ProgramManagment.jsx
@@ -1,23 +1,36 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import { useMediaQuery } from 'react-responsive';
 import { SvgSelector } from '../assets/images/SvgSelector';
 import s from '../Managment.module.scss'
 
 
 
 export const ProgramManagment = (props) => {
-    const [toggleActiveLeft, setToggleActiveLeft] = useState('active');
-    const [toggleActiveRight, setToggleActiveRight] = useState('active');
+    const [toggleActiveLeft, setToggleActiveLeft] = useState(true);
+    const [toggleActiveRight, setToggleActiveRight] = useState(true);
     const [contentHeightLeft, setContentHeightLeft] = useState('');
     const [contentHeightRight, setContentHeightRight] = useState('');
     const content = useRef();
+    const isMobile = useMediaQuery({
+        query: `(max-width: 460px)`
+    })
+
+    useEffect(() => {
+        if (isMobile) {
+            setToggleActiveLeft(false)
+            setContentHeightLeft('0px')
+            setToggleActiveRight(false)
+            setContentHeightRight('0px')
+        }
+    }, [isMobile])
 
     const toggleAccordion = (e) => {
         if (e.target.innerHTML === '1 модуль') {
-            setToggleActiveLeft(toggleActiveLeft === '' ? 'active' : '');
-            setContentHeightLeft(toggleActiveLeft === 'active' ? '0px' : `${content.current.scrollHeight + 50}px`)
+            setToggleActiveLeft(!toggleActiveLeft);
+            setContentHeightLeft(toggleActiveLeft ? '0px' : `${content.current.scrollHeight + 50}px`)
         } else {
-            setToggleActiveRight(toggleActiveRight === '' ? 'active' : '');
-            setContentHeightRight(toggleActiveRight === 'active' ? '0px' : `${content.current.scrollHeight + 50}px`)
+            setToggleActiveRight(!toggleActiveRight);
+            setContentHeightRight(toggleActiveRight ? '0px' : `${content.current.scrollHeight + 50}px`)
         }
     }
 
@@ -26,18 +39,18 @@ export const ProgramManagment = (props) => {
         <section className={s.managment}>
             <div className={s.moduleStyle}>
                 <div className={s.toggleSymbol}>
-                    {toggleActiveLeft === 'active' ? <SvgSelector id='minus' /> : <SvgSelector id='plus' />}
+                    {toggleActiveLeft ? <SvgSelector id='minus' /> : <SvgSelector id='plus' />}
                 </div>
-                <div onClick={toggleAccordion} className={`${s.moduleStep} ${toggleActiveLeft}`}>1 модуль</div>
+                <div onClick={toggleAccordion} className={`${s.moduleStep} ${toggleActiveLeft && s.active}`}>1 модуль</div>
                 <ul ref={content} style={{ maxHeight: `${contentHeightLeft}` }}>
                     {props.subjects.map((el, i) => { return (i < 5) && <li key={i}>{el}</li> })}
                 </ul>
             </div>
             {props.subjects.length > 5 && <div className={s.moduleStyle}>
                 <div className={s.toggleSymbol}>
-                    {toggleActiveRight === 'active' ? <SvgSelector id='minus' /> : <SvgSelector id='plus' />}
+                    {toggleActiveRight ? <SvgSelector id='minus' /> : <SvgSelector id='plus' />}
                 </div>
-                <div onClick={toggleAccordion} className={`${s.moduleStep} ${toggleActiveRight}`}>2 модуль</div>
+                <div onClick={toggleAccordion} className={`${s.moduleStep} ${toggleActiveRight && s.active}`}>2 модуль</div>
                 <ul ref={content} style={{ maxHeight: `${contentHeightRight}` }}>
                     {props.subjects.map((el, i) => { return (i > 4) && <li key={i}>{el}</li> })}
                 </ul>
